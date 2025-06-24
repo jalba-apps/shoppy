60dd7f0e4c169be0a4d0971c14ce2eb6b316f373
Jose Armando <jose_armandoam@outlook.es>
2025-06-24 17:21:20 +0200

    Initial Commit

---
+++ shoppy/shoppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppyApp.swift
+//  shoppy
+//
+//  Created by Jose armando Albarado Mamani on 24/6/25.
+//
+
+import SwiftUI
+
+@main
+struct shoppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shoppyTests/shoppyTests.swift
@@ -0,0 +1,17 @@
+//
+//  shoppyTests.swift
+//  shoppyTests
+//
+//  Created by Jose armando Albarado Mamani on 24/6/25.
+//
+
+import Testing
+@testable import shoppy
+
+struct shoppyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
